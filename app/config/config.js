c4747d3ae5819af6acbfdd3ba66a0894e19adda9
Alan Lavintman <alan.lavintman@gmail.com>
2015-12-23 16:37:40 +0200

    Changed the way config works

--- app/config/config.js
+++ app/config/config.js
@@ -3,23 +3,35 @@
  */
 'use strict';
 var os = require('os');
+var process = require('process');
+
+var lookup = function(variable, envValue, defaultValue){
+    if (envValue){
+        console.log("Configuring variable: " + variable + " taken from environment with value: " + envValue);
+        defaultValue = envValue;
+    } else {
+        console.log("Configuring variable: " + variable + " taken from default value: " + defaultValue);
+    };
+    return defaultValue;
+}
+
 module.exports = {
     tokenValidTime: 600000, // token expiration every 10 minutes.
     pid: os.pid,
     hostname: os.hostname,
     service: "auth-service",
-    db: 'mongodb://ec2-52-19-217-56.eu-west-1.compute.amazonaws.com:27017/user_db',
+    db: lookup('mongo_db', process.env.MONGO_DB, 'mongodb://ec2-52-19-217-56.eu-west-1.compute.amazonaws.com:27017/user_db'),
     kafka: {
-        zk: "stg-kafka.bubbleyou.com:2181",
-        clientId: "auth-service",
+        zk: lookup('zk', process.env.ZK, 'stg-kafka.bubbleyou.com:2181'),
+        clientId: lookup('client_id', process.env.CLIENT_ID, 'auth-service'),
         zkOptions: {
-            sessionTimeout: 30000,
-            spinDelay : 2000,
-            retries : 3
+            sessionTimeout: lookup('kafka.session_timeout', process.env.ZK_SESSION_TIMEOUT, 30000),
+            spinDelay : lookup('kafka.spin_delay', process.env.ZK_SPIN_DELAY, 2000) ,
+            retries : lookup('kafka.retry', process.env.ZK_RETRY, 3)
         },
         topics: {
-            service_status_topic: "service_status",
-            user_login_topic: "user_login"
+            service_status_topic: lookup('kafka.topic.status', process.env.KAFKA_STATUS_TOPIC, "service_status"),
+            user_login_topic: lookup('kafka.topic.login', process.env.KAFKA_LOGIN_TOPIC, "user_login")
         }
     },
     status: {}

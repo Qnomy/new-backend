b494feb925f72ce8e35895f60b967ff5c74ee81f
Yehuda Tuvia <ytuvia@gmail.com>
2016-08-12 14:14:33 +0300

    beginning of vschat service

---
+++ app/models/vschat.js
@@ -0,0 +1,39 @@
+var mongoose = require('mongoose');
+var schema = mongoose.Schema;
+var async = require('async');
+
+var vsChatMessageSchema = mongoose.schema({
+	from: String,
+	body: String,
+	created_date: { type: Date, default: Date.now }
+})
+
+var vsChatRoomSchema = mongoose.Schema({
+	members: [String],
+	messages: [vsChatMessageSchema]
+});
+
+var vsChatRoomModel = new mongoose.model("vschatroom", vsChatRoomSchema);
+
+function getRoom(members, cb){
+	vsChatRoomSchema.findOne({members:members}, function(err, room){
+		if(!room){
+			var room = new vsChatRoomModel({
+				members: members
+			});
+			room.save(function(err){
+				return cb(err, room);
+			})
+		}else{
+			return cb(err, room);
+		}
+	});
+}
+
+function addMessage(room, user, body, cb){
+	room.messages.push({
+		from: user._id,
+		body: body
+	});
+
+}
